0429cae0d8031d18d52b6cc02f2b5ffa1b2a329b
李梦轩 <88953940+lmx0828@users.noreply.github.com>
2023-10-09 16:17:32 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -24,7 +24,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: '50d96048d4994c91b35c4ebc43ffb51e',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
